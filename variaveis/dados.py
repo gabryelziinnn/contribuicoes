5d78318f1c51d9740b05c2035812944f74ff26f0
gabryelziinnn <gabryelziinnnnn@gmail.com>
2023-01-21 16:27:46 -0300

    ajusta o nome da chave

--- variaveis/dados.py
+++ variaveis/dados.py
@@ -6,9 +6,7 @@ instancia = 'ami-0574da719dca65348'
 
 tipo_de_instancia = 't2.micro'
 
-chave_publica_gab = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCwx1/gm/WT54qQlFmCR0/DmVwOsm6Dpj8viHjV/5N5LhfXe4wpSeEzjVZVbYs5NfkwbogXrTSTO6MXEfaLwosL2WJQUKs52CWCjOUumnUPBeK4Y2WtNC/cmudNcGj/ym+AtpokXQt2sgeVIwqKBLuZMu2tJERzUMB1ELJpp3XyPQEho3DrJd+cbsWTvSrbvLV+1qvg21G9RsVe7Snac9U0Pe7HYO4LT2gpUUGejOOrFxACo+hcLgqhPPBP+ATYuGPIadBi/cFAw5rvjsfffykKbxGJhIHrYKoPx7dXmxhVwZhhR32WG29ozLBT3RrQ5e48Hi2AU41eAbHb1V8Bbx5xL9aYeYwqTrp9jbMTtOgkNdOmF8wHsGfxB8XbmuCnpembNJFIL8j2t2cWBKYJh8aT5yOL4Uw6aHVkpRdAXfTWQv64xYpGO2Ze3QPcw7azPMP2u5YCpVO8G/3o7BtFJFi5sglU14OtxGKLRabaXq/VlJBr2GBdRqwKh6zV0E6ZmeE= gabryel@gab-devops'
-
-nome_da_chave = 'gabryel-note'
+nome_da_chave = 'defina sua chave já existente aqui'
 
 zona_de_disponibilidade = 'us-east-1a'
 
